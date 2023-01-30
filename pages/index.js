c2ca1a6d7b4528a8ce8e0ffeb8c713bb23674d6d
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2023-01-30 11:32:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,11 @@ export async function getStaticProps() {
 
 export default function Home({allPostsData}) {
   return (
-    <Layout>
+    <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
