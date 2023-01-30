63581f686af762802e9e7e7fbb72900ddabb358c
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2023-01-30 10:53:29 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -25,41 +25,41 @@ export default function Layout({children,home}) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt=""
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+//       <header className={styles.header}>
+//         {home ? (
+//           <>
+//             <Image
+//               priority
+//               src="/images/profile.jpg"
+//               className={utilStyles.borderCircle}
+//               height={144}
+//               width={144}
+//               alt=""
+//             />
+//             <h1 className={utilStyles.heading2Xl}>{name}</h1>
+//           </>
+//         ) : (
+//           <>
+//             <Link href="/">
+//               <a>
+//                 <Image
+//                   priority
+//                   src="/images/profile.jpg"
+//                   className={utilStyles.borderCircle}
+//                   height={108}
+//                   width={108}
+//                   alt=""
+//                 />
+//               </a>
+//             </Link>
+//             <h2 className={utilStyles.headingLg}>
+//               <Link href="/">
+//                 <a className={utilStyles.colorInherit}>{name}</a>
+//               </Link>
+//             </h2>
+//           </>
+//         )}
+//       </header>
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
