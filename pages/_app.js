a6ecde682a1ece74bd96375b5dd2c614d2fa2f81
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2023-01-30 10:40:21 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import Navbar from "../components/Navbar";
 import '../styles/global.css';
 export default function App({Component,pageProps}){
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
