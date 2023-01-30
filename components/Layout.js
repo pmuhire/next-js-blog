e53175b52a3867d5177a41a5a9e298d192e46a15
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2023-01-30 11:03:03 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export const siteTitle = 'CodingGuru'
 export default function Layout({children,home}) {
   return (
     <div className={styles.container}>
-      <Head>
+      <Head home>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
@@ -25,41 +25,6 @@ export default function Layout({children,home}) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-//       <header className={styles.header}>
-//         {home ? (
-//           <>
-//             <Image
-//               priority
-//               src="/images/profile.jpg"
-//               className={utilStyles.borderCircle}
-//               height={144}
-//               width={144}
-//               alt=""
-//             />
-//             <h1 className={utilStyles.heading2Xl}>{name}</h1>
-//           </>
-//         ) : (
-//           <>
-//             <Link href="/">
-//               <a>
-//                 <Image
-//                   priority
-//                   src="/images/profile.jpg"
-//                   className={utilStyles.borderCircle}
-//                   height={108}
-//                   width={108}
-//                   alt=""
-//                 />
-//               </a>
-//             </Link>
-//             <h2 className={utilStyles.headingLg}>
-//               <Link href="/">
-//                 <a className={utilStyles.colorInherit}>{name}</a>
-//               </Link>
-//             </h2>
-//           </>
-//         )}
-//       </header>
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
